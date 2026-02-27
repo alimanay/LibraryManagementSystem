940fa55ce5c8d62c0280879bcf6307e32b77ff75
Ali manay <150849054+alimanay@users.noreply.github.com>
2026-02-27 06:15:35 +0300

    İnput Kontrolleri sağlandı

--- WebUI/Entites/Dtos/UserDtos/UpdateUserDto.cs
+++ WebUI/Entites/Dtos/UserDtos/UpdateUserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,17 @@ namespace Entites.Dtos.UserDtos
     public class UpdateUserDto
     {
         public int UserId { get; set; }
+        [Required(ErrorMessage = "Ad alanı boş bırakılamaz")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Soyad alanı boş bırakılamaz")]
         public string Surname { get; set; }
-        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email boş bırakılamaz")]
+        [EmailAddress(ErrorMessage = "Geçerli bir email giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon boş bırakılamaz")]
+        [MinLength(11, ErrorMessage = "Telefon numarası 11 haneli olmalı"), MaxLength(11)]
         public string PhoneNumber { get; set; }
         public bool IsActive { get; set; }
     }
